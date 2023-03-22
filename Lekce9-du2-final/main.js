9683d4f9ec18ccb603e5fbdf75cbedf34c6060e0
Barbora Lendacka <123261265+JavaBorka@users.noreply.github.com>
2023-03-22 22:37:26 +0100

    Add files via upload

---
+++ Lekce9-du2-final/main.js
@@ -0,0 +1,29 @@
+console.log('Všetko baví')
+
+
+let user = {
+	email: 'bartolomej.rozumbrada@gmail.com',
+	password: 'vimzenicnevim',
+	name: 'Bartoloměj',
+}
+
+const formular = document.querySelector('form')
+
+formular.addEventListener('submit', (event) => {
+    event.preventDefault()
+
+    let emailInput = document.querySelector('#email')
+    let passwordInput = document.querySelector('#password')
+    let psw = passwordInput.value
+    let result = document.querySelector('#result')
+    let container = document.querySelector('.container')
+
+    if (emailInput.value === user.email && passwordInput.value === user.password) {
+        container.innerHTML = `<p> Přihlášený uživatel: ${user.name}</p>`
+    } else {
+        result.innerHTML = `<p><b> Neplatné přihlašovací údaje </b></p>`
+        passwordInput.value = '' // toto je OK
+        // psw = '' // toto není OK
+    }
+})
+
